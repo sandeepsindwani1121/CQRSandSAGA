ab30a307052ee61cde16eba0cfa8800863a2547e
manya1121 <manya1121@outlook.com>
2021-02-28 19:30:48 +0400

    RabbitMQ and Repositery pattern

---
+++ rabbitmq/Program.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace rabbitmq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "locationSampleQueue",
+                        durable: false, 
+                        exclusive: false, 
+                        autoDelete: false,
+                        arguments: null);
+                    string message = "Latitude: 12345678.99, Longitude: 34252677.77 and Time: " + DateTime.Now;
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(exchange: "", routingKey: "locationSampleQueue",
+                                             basicProperties: null,
+                                             body: body);
+
+                    //consume MSMQ
+                    Int16 messageCount = Convert.ToInt16(channel.MessageCount("locationSampleQueue"));
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" Location received: " + message);
+                    };
+                }
+            }
+        }
+
+        public class Location
+        {
+            public DateTime Date { get; set; }
+            public double Latitude { get; set; }
+            public double Longitude { get; set; }
+        }
+
+    }
+}
---
+++ repositorypattern/Program.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace repositorypattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public interface IRepositery<T>
+    {
+        bool Add(T obj); 
+        bool Update(T obj,int id);
+        List<T> GetQuery();
+
+    }
+
+    public class BaseRepositery<T> : IRepositery<T> where T : class
+    {
+        DbContext _dbContext;
+        DbSet<T> _dbSet;
+        public BaseRepositery(DbContext dbContext)
+        {
+            this._dbContext = dbContext;
+            this._dbSet = _dbContext.Set<T>();
+        }
+
+        public bool Add(T obj)
+        {
+            this._dbSet.Add(obj);
+            return true;
+        }
+
+        public List<T> GetQuery()
+        {
+            return this._dbSet.ToList(); ;
+        }
+
+        public bool Update(T obj, int id)
+        {
+            this._dbSet.Update(obj);
+            return true;
+        }
+    }
+
+    public class Customer
+    {
+        public string Name { get; set; }
+    }
+    public class Supplier
+    {
+        public string Name { get; set; }
+    }
+    public interface IUnitOfWork
+    {
+        IRepositery<Customer> Customers { get; }
+        IRepositery<Supplier> Suppliers { get; }
+    }
+
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        DbContext _dbContext;
+        BaseRepositery<Customer> _customer;
+        BaseRepositery<Supplier> _supplier;
+        public UnitOfWork(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IRepositery<Customer> Customers
+        {
+            get
+            {
+                return _customer ??
+                    (_customer = new BaseRepositery<Customer>(_dbContext));
+            }
+        }
+
+        public IRepositery<Supplier> Suppliers
+        {
+            get
+            {
+                return _supplier ??
+                    (_supplier = new BaseRepositery<Supplier>(_dbContext));
+            }
+        }
+    }
+
+}
